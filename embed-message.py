c2b7c10172ec08e3b675e55b648adec57d1303d0
yan-xq <119748459+yan-xq@users.noreply.github.com>
2022-12-04 14:01:05 +0200

    Update embed-message.py

--- embed-message.py
+++ embed-message.py
@@ -13,4 +13,18 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('.help'))
     print('connected')
 
+@bot.command(aliases=['embed'])#aliases can rename your command
+async def __embed(ctx):
+    embed = discord.Embed(
+        title='your title embed',
+        description='your description',
+        color=discord.Color.red()#you can choose another embed color
+    )
+    embed.add_field(name='your field name', value='your field description or value', inline='False or True')#field
+    embed.set_footer(text='your text', icon_url='your link for icon')#footer
+    embed.set_author(name='your author name and also you can write ctx.message.author', icon_url='your link for icon')#author
+    embed.set_image(url='your link for image')#image
+    embed.set_thumbnail(url='your link for image')#thumbnail
+    await ctx.send(embed = embed)
 
+bot.run('your token')
