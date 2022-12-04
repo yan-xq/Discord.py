cfd98e6dd4a4230ffea226603c7ef0b0f86b7d73
yan-xq <119748459+yan-xq@users.noreply.github.com>
2022-12-04 13:43:59 +0200

    Add files via upload

---
+++ change-activity.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+#intents
+intents = discord.Intents.all()
+
+#bot
+bot = commands.Bot(command_prefix = '.', help_command=None, intents = intents)
+
+#activity
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('your activity'))
